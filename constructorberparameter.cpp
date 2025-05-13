b429588f1c832e3d2d37b8901a6e4946924f7290
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-05-13 18:54:20 +0700

    Menambahkan isi dari constructor

---
+++ constructorberparameter.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class mahasiswa{
+    public:
+        mahasiswa(int nim, string nama);// Constructor Berparameter
+};
+
+//Definisi contructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama){
+    cout << "Constructor yang terpanggil" <<endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nama : " << nama << endl; 
+}
