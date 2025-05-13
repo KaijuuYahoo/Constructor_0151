f3ce9210cf77c7f0e273688ec071b11511263334
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-05-13 19:21:57 +0700

    Penambahan class dengan nama buku

---
+++ this.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class buku{
+    string judul;
+    public:
+        string setget(string jdl){
+            //nilai parameter 'judul' untuk member variabel judul
+            this -> judul = jdl;
+            // return variabel judul
+            return ju
+        }
+
+};
