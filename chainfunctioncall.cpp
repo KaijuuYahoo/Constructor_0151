8022fd08c53762844784b14f126b6574b204bdbd
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-05-13 19:27:03 +0700

    Penambahan class bernama buku

---
+++ chainfunctioncall.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class buku{
+    string judul;
+    public:
+        buku setJudul(string judul){
+            this -> judul = judul;
+            return *this; //chain Function
+        }
+
+        string getJudul(){
+            return this ->judul;
+        }
+};
+
+int main
